566dbdbb694d037cc3d034f75995121844e437a0
陈泰任 <867804244@qq.com>
2018-06-06 10:12:32 +0800

    菜单隐藏效果修改

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -94,7 +94,9 @@
         <!-- 底部固定区域 -->
         © orinfyAdmin
     </div>
+    <!-- 移动端菜单弹出按钮 -->
     <div class="site-tree-mobile layui-hide"><i class="layui-icon">&#xe602;</i></div>
+    <!--移动端菜单弹出阴影效果-->
     <div class="site-mobile-shade"></div>
 </div>
 <script src="{{ asset('layadmin/layui/layui.js') }}"></script>
@@ -108,7 +110,7 @@
         $ = layui.jquery;
 
         var cms = layui.cms('left-nav', 'top-tab');
-
+        // 添加菜单
         cms.addNav([
             {id: 1, pid: 0, node: '主页', url: 'main.html'},
             {id: 7, pid: 0, node: '登录', url: 'signin.html'},
@@ -121,34 +123,10 @@
         ], 0, 'id', 'pid', 'node', 'url');
 
         cms.bind(60 + 41 + 20 + 44); //头部高度 + 顶部切换卡标题高度 + 顶部切换卡内容padding + 底部高度
-
+        // 默认打开窗口
         cms.clickNavId(1);
 
-        $(".hideMenu").on("click",function(){
-            var oClass=$(".layui-side").hasClass("mini");
-            if(!oClass){
-                $(".layui-side").animate({
-                    width:"0"
-                })
-                $(".layui-body").animate({
-                    left:"0"
-                })
-                $(".layui-footer").animate({
-                    left:"0"
-                })
-            }else{
-                $(".layui-side").animate({
-                    width:"200px"
-                })
-                $(".layui-body").animate({
-                    left:"200"
-                })
-                $(".layui-footer").animate({
-                    left:"200"
-                })
-            }
-            $(".layui-side").toggleClass("mini");
-        });
+
         //mini 模式下添加 tips
         // $('.layui-nav-tree a').hover(function(){
         //     if($(".layui-side").hasClass('mini')){
@@ -158,30 +136,37 @@
         // },function(){
         //     layer.closeAll('tips');
         // });
-
+        // pc模式下菜单隐藏效果
+        $(".hideMenu").on("click",function(){
+            $('.layui-layout-admin').toggleClass('menu-hide');
+        });
+        // 移动端下菜单隐藏效果
         $('.site-tree-mobile').click(function () {
-            $('.layui-side').toggleClass('site-mobile');
-            $('.site-mobile-shade').toggleClass('site-mobile');
+            $('.layui-layout-admin').toggleClass('site-mobile');
         });
+        // 移动端菜单按钮是否显示
         $('.site-mobile-shade').click(function(){
-            $('.layui-side').toggleClass('site-mobile');
-            $('.site-mobile-shade').toggleClass('site-mobile');
+            $('.layui-layout-admin').toggleClass('site-mobile');
         });
+        $(window).resize(function() {
+            // 屏幕调整清除多余class
+            $('.layui-layout-admin').removeClass('site-mobile').removeClass('menu-hide');
+        })
 
 
-        layer.open({
-            type: 1
-            ,title: false //不显示标题栏
-            ,closeBtn: false
-            ,area: '300px;'
-            ,shade: 0.8
-            ,id: 'LAY_layuipro' //设定一个id，防止重复弹出
-            ,btn: ['知道了']
-            ,btnAlign: 'c'
-            ,moveType: 1 //拖拽模式，0或者1
-            ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">' +
-            '注意啦，各位亲！<br>当前项目部署在亚马逊的免费服务器上，而且是国外的所以速度有点蜗牛<br><br>当前VIP用户所属的是管理员角色，请不要对此角色进行停用修改删除，否则重新登录之后就看不到东西了<br><br>新增菜单是需要授权才可以看见的，所以目前新增菜单是看不见效果的<br></div>'
-        });
+        // layer.open({
+        //     type: 1
+        //     ,title: false //不显示标题栏
+        //     ,closeBtn: false
+        //     ,area: '300px;'
+        //     ,shade: 0.8
+        //     ,id: 'LAY_layuipro' //设定一个id，防止重复弹出
+        //     ,btn: ['知道了']
+        //     ,btnAlign: 'c'
+        //     ,moveType: 1 //拖拽模式，0或者1
+        //     ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">' +
+        //     '注意啦，各位亲！<br>当前项目部署在亚马逊的免费服务器上，而且是国外的所以速度有点蜗牛<br><br>当前VIP用户所属的是管理员角色，请不要对此角色进行停用修改删除，否则重新登录之后就看不到东西了<br><br>新增菜单是需要授权才可以看见的，所以目前新增菜单是看不见效果的<br></div>'
+        // });
 
     });
 </script>
