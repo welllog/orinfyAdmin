ae0d5fa2a38d183235a3a39582791906f354343f
ctr <orinfy@foxmail.com>
2020-06-30 10:11:59 +0800

    logout error fix

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Exceptions\OrException;
 use App\Service\UserService;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
